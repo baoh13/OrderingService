288a9c3c84a5581a430e2d87a60f470e4b5136ff
baoh13 <dzacu1a@gmail.com>
2016-09-03 16:29:55 +0100

    Add the OrderingService proj to the repository

---
+++ OrderingService/Controllers/OrdersController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Http;
+
+namespace OrderingService.Controllers
+{
+    public class OrdersController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            try
+            {
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+                
+                return InternalServerError();
+            }
+        }
+    }
+}
---
+++ OrderingService/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderingService.Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+        public int Quantity { get; set; } 
+    }
+}
